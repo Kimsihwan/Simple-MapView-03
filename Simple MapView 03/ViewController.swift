c8674973e47b668262c26f64c961b60ec8baefc6
D7702_10 <d7702_10@d7702-10ui-imac.local>
2018-06-05 12:23:30 +0900

    핀설정

--- Simple MapView 03/ViewController.swift
+++ Simple MapView 03/ViewController.swift
@@ -15,7 +15,43 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //번개반점
+        //35.167578, 129.070568
+        //부산광역시 부산진구 양정동 418-282
+        //051-852-9969
+        
+        //DIT
+        //35.165782, 129.072529
+        //부산광역시 부산진구 양정동 양지로 54
+        //051-852-0011
+        
+        //중심 위도, 경도로 설정
+        let center = CLLocationCoordinate2D(latitude: 35.165782, longitude: 129.072529)
+        
+        //번개반점
+        let loc01 = CLLocationCoordinate2D(latitude: 35.167578, longitude: 129.070568)
+        
+        //반경(zoom in, zoom out) 설정
+        let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        
+        //region 객체는 center, span을 묶는것
+        let region = MKCoordinateRegion(center: center, span: span)
+        
+        //지도에 표시하기
+        myMapView.setRegion(region, animated: true)
+        
+        ////annotation 표시하기
+        let pin01 = MKPointAnnotation()
+        pin01.coordinate = loc01
+        pin01.title = "번개반점"
+        pin01.subtitle = "부산광역시 부산진구 양정동 418-282"
+        myMapView.addAnnotation(pin01)
+        
+        let pin02 = MKPointAnnotation()
+        pin02.coordinate = center
+        pin02.title = "동의과학대"
+        pin02.subtitle = "부산광역시 부산진구 양정동 양지로 54"
+        myMapView.addAnnotation(pin02)
     }
-
 }
-
