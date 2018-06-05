c2e136f936b49246070e16296c6dfe6bfcec4819
D7702_10 <d7702_10@d7702-10ui-imac.local>
2018-06-05 15:12:52 +0900

    핀 6개

--- Simple MapView 03/ViewController.swift
+++ Simple MapView 03/ViewController.swift
@@ -12,6 +12,14 @@ import MapKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var myMapView: MKMapView!
+    
+    var foodStoreAddress = ["부산광역시 부산진구 양정2동 336-17",
+                            "부산광역시 부산진구 양정동 418-282",
+                            "부산광역시 부산진구 양정동 393-18",
+                            "부산광역시 부산진구 양정1동 356-22",
+                            "부산광역시 부산진구 양정1동 350-1",
+                            "부산광역시 부산진구 양정동 353-38"]
+    var foodStoreTitle = ["양정밀면", "번개반점", "아딸", "왕짜장", "늘해랑", "홍콩반점"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -29,9 +37,6 @@ class ViewController: UIViewController {
         //중심 위도, 경도로 설정
         let center = CLLocationCoordinate2D(latitude: 35.165782, longitude: 129.072529)
         
-        //번개반점
-        let loc01 = CLLocationCoordinate2D(latitude: 35.167578, longitude: 129.070568)
-        
         //반경(zoom in, zoom out) 설정
         let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
         
@@ -42,11 +47,11 @@ class ViewController: UIViewController {
         myMapView.setRegion(region, animated: true)
         
         ////annotation 표시하기
-        let pin01 = MKPointAnnotation()
-        pin01.coordinate = loc01
-        pin01.title = "번개반점"
-        pin01.subtitle = "부산광역시 부산진구 양정동 418-282"
-        myMapView.addAnnotation(pin01)
+//        let pin01 = MKPointAnnotation()
+//        pin01.coordinate = loc01
+//        pin01.title = "번개반점"
+//        pin01.subtitle = "부산광역시 부산진구 양정동 418-282"
+//        myMapView.addAnnotation(pin01)
         
         let pin02 = MKPointAnnotation()
         pin02.coordinate = center
@@ -55,32 +60,51 @@ class ViewController: UIViewController {
         myMapView.addAnnotation(pin02)
         
         //// geocoding 클로저
-        let addr = "부산광역시 부산진구 양정1동 350-1"
-        let geocoder = CLGeocoder()
-        geocoder.geocodeAddressString(addr) {
-            
-            (placemarks: [CLPlacemark]?, error: Error?) -> Void in
-            if let error = error {
-                print(error)
-                return
-            } else {
-                print("nil 발생")
-            }
-            if let placemarks = placemarks {
-            
-                let placemarks = placemarks[0]
-//                print(placemarks.location!)
-//                print(placemarks.name!)
-//                print(placemarks.postalCode!)
-//                print(placemarks.country!)
-                let loc02 = placemarks.location?.coordinate
-                let pin03 = MKPointAnnotation()
-                pin03.coordinate = loc02!
-                pin03.title = "늘해랑"
-                pin03.subtitle = addr
-                self.myMapView.addAnnotation(pin03)
-            } else {
-                print("nil 발생")
+//        let addr = "부산광역시 부산진구 양정1동 350-1"
+//        let geocoder = CLGeocoder()
+//        geocoder.geocodeAddressString(addr) {
+//
+//            (placemarks: [CLPlacemark]?, error: Error?) -> Void in
+//            if let error = error {
+//                print(error)
+//                return
+//            } else {
+//                print("nil 발생")
+//            }
+//            if let placemarks = placemarks {
+//
+//                let placemarks = placemarks[0]
+////                print(placemarks.location!)
+////                print(placemarks.name!)
+////                print(placemarks.postalCode!)
+////                print(placemarks.country!)
+//                let loc02 = placemarks.location?.coordinate
+//                let pin03 = MKPointAnnotation()
+//                pin03.coordinate = loc02!
+//                pin03.title = "늘해랑"
+//                pin03.subtitle = addr
+//                self.myMapView.addAnnotation(pin03)
+//            } else {
+//                print("nil 발생")
+//            }
+//        }
+
+        
+        for addr in foodStoreAddress {
+            let counter = self.foodStoreAddress.count
+            let geocoder = CLGeocoder()
+            geocoder.geocodeAddressString(addr) {
+                (placemarks: [CLPlacemark]?, error: Error?) -> Void in
+                if let myError = error {
+                    print(myError)
+                    return
+                }
+                let myPlacemarks = placemarks![0]
+                let loc = myPlacemarks.location?.coordinate
+                let pin = MKPointAnnotation()
+                pin.coordinate = loc!
+                self.myMapView.addAnnotation(pin)
+                
             }
         }
     }
